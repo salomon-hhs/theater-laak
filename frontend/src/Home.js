9107bf1fe2325c60836612c24ffeb1eaee328de1
salomon-hhs <21092915@student.hhs.nl>
2022-12-19 13:34:10 +0100

    Initial commit - Project setup

---
+++ frontend/src/Home.js
@@ -0,0 +1,8 @@
+function App() {
+  return <>
+      <h1 className="text-3xl">Theater Laak</h1>
+      <p>Welkom op de website van Theater Laak. Deze website is momenteel onder constructie.</p>
+    </>;
+}
+
+export default App;
