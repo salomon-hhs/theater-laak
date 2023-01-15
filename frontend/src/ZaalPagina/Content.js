b676eca29167c5a40ceaee798f4f6b5964d73705
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-01-15 21:09:09 +0100

    Zaal component unfinished

---
+++ frontend/src/ZaalPagina/Content.js
@@ -0,0 +1,10 @@
+export function ZaalCoponent(){
+    return <>
+    <div>
+        <h2></h2>
+        <form>
+        <label for="fname">Zaal nummer</label>
+        </form> 
+    </div>
+    </>
+}
