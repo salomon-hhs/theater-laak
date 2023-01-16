0b205e78ffea247ba91a64e6439f5d23758e07b4
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-01-16 10:03:07 +0100

    Evenement Pagina

---
+++ frontend/src/EvenementPagina/ContentEV.js
@@ -0,0 +1,44 @@
+export function EPComponent(){
+
+  return <>
+  
+ <h2 className="ml-44 mb-2">Alle evenementen</h2>
+ <div className="flex flex-wrap justify-center content-around" id="flex1">
+            <div className=" mx-2 my-6">
+              <img className="Event_img rounded-2xl" src="https://cdn.pixabay.com/photo/2016/11/08/05/15/ramayana-festival-1807516_1280.jpg" alt="Link en een foto van een asian celebration evenement"/>
+              <h3 className="text-left">Chinese New Years</h3>
+              <div className="text-left"><a id="Chinese_Ney_Years" className="meerInfoLink" href="/">Meer weten over Celebration of some sort</a></div>
+            </div>
+            
+            <div className="mx-2 my-6">
+             <img className="Event_img rounded-2xl" src="https://cdn.pixabay.com/photo/2016/09/13/07/35/walking-dead-1666584_1280.jpg" alt="Link en een foto van de walking dead evenement"/>
+             <h3 className="text-left">The Walking Dead</h3>
+             <div className="text-left"><a id="The_Walking_Dead" className="meerInfoLink" href="/">Meer weten over Celebration of some sort</a></div>
+            </div>
+
+            <div className="mx-2 my-6">
+              <img className="Event_img rounded-2xl" src="https://cdn.pixabay.com/photo/2014/08/29/05/00/dance-430554_1280.jpg" alt="" />
+              <h3 className="text-left">Step Up 5</h3>
+              <div className="text-left"><a id="Step_Up" className="meerInfoLink" href="/">Meer weten over Celebration of some sort</a></div>
+            </div>
+
+            <div className="mx-2 my-6">
+              <img className=" Event_img rounded-2xl" src="https://cdn.pixabay.com/photo/2016/11/08/05/15/ramayana-festival-1807516_1280.jpg" alt="Link en een foto van een asian celebration evenement"/>
+              <h3 className="text-left">Celebration of some sort</h3>
+              <div className="text-left"><a id="Celebration_of_some_sort" className="meerInfoLink" href="/">Meer weten over Celebration of some sort</a></div>
+            </div>
+            
+            <div className="mx-2 my-6">
+             <img className="Event_img rounded-2xl" src="https://cdn.pixabay.com/photo/2016/09/13/07/35/walking-dead-1666584_1280.jpg" alt="Link en een foto van de walking dead evenement"/>
+             <h3 className="text-left">Dead Man Walking</h3>
+             <div className="text-left"><a id="Dead_Man_Walking" className="meerInfoLink" href="/">Meer weten over Celebration of some sort</a></div>
+            </div>
+
+            <div className="mx-2 my-6">
+              <img className="Event_img rounded-2xl" src="https://cdn.pixabay.com/photo/2014/08/29/05/00/dance-430554_1280.jpg" alt="" />
+              <h3 className="text-left">Step Up Another Sequel</h3>
+              <div className="text-left"><a id="Step Up Another Sequel" className="meerInfoLink" href="/">Meer weten over Celebration of some sort</a></div>
+            </div>
+  </div> </>
+  
+}
---
+++ frontend/src/HomePagina/ContentHP.js
@@ -0,0 +1,10 @@
+import { EvenementenHP } from "./EvenementHP"
+import { ZaalHurenHP } from "./ZaalHuren"
+export function ContentHP(){
+    return <>
+    <body>
+    <EvenementenHP/>
+    <ZaalHurenHP/>
+    </body>
+</>
+}
