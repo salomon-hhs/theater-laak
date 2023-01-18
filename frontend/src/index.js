8ef2f98f348fa8b67341986e527a2cc164e2f7ee
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-01-18 16:05:14 +0100

    Ticket page completed and components moved to EP

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,9 +11,8 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 import PasswVergeten from './Components/PasswVergeten';
 import reportWebVitals from './reportWebVitals';
-import { EvenementenPagina } from './EvenementPagina/ContentEV';
+import { EvenementenPagina, TicketPagina } from './EvenementPagina/ContentEV';
 import { HomePagina } from './HomePagina/ContentHP';
-import { TicketPagina } from './TicketPagina/TicketComponent';
 
 const router = createBrowserRouter([
   {
