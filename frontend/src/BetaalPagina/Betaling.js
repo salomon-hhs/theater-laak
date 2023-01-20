f095260ac39d1c5620350d11ae8ac41240044b2d
Sander <sander.heidenis@gmail.com>
2023-01-20 22:43:01 +0100

    setup?

--- frontend/src/BetaalPagina/Betaling.js
+++ frontend/src/BetaalPagina/Betaling.js
@@ -2,6 +2,7 @@ import react, {useEffect, useState} from 'react'
 
 export default function Betaling() {
     const success = Request.body.success();
+    //get id as well
 
     return (
         <>
